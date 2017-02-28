a0174454ac7579fc9aa354910b41dce23edbb33a
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-02-27 21:55:40 -0800

    Add stuff

---
+++ script.js
@@ -0,0 +1,26 @@
+function setBackground(url) {
+  $(".background").css("background-image", `url("${url}")`)
+}
+
+function sendGifRequest(search) {
+  $.ajax({
+    url: "http://api.giphy.com/v1/gifs/search",
+    type: "get",
+    data: {
+      api_key: "dc6zaTOxFJmzC",
+      q: search
+    }
+  }).done(function(response) {
+    console.log(response)
+    setBackground(response.data[0].images.downsized.url)
+  })
+}
+
+
+$(document).ready(function() {
+
+  $(".cool-input").addClass("shown")
+  $(".cool-input").change(function() {
+    sendGifRequest($(this).val())
+  })
+})
